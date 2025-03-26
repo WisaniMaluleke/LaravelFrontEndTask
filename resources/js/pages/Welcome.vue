d91cff5c6d57a46ecf39110ab35366c8a6c3b38a
WisaniMaluleke <wislim.aj@gmail.com>
2025-03-26 12:52:46 +0200

    navbar background color

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-fluid">
 
-    <nav class="fixed w-full z-20 top-0 start-0 border-b border-gray-500 dark:border-gray-600">
+    <nav class="app-background fixed w-full z-20 top-0 start-0 border-b border-gray-500 dark:border-gray-600">
       <div class="flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
           <img :src="imageUrl" class="h-8" alt="Flowbite Logo">
